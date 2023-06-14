4b589566c2f7acc21d32ec863a5fba57992dee60
Junior Rantila <junior.rantila@gmail.com>
2023-06-14 16:32:41 +0200

    CJ: Fix bogus ACStack_alloc
    Seems we cannot alloca even if it is always inlined :(

--- src/AC/Stack.h
+++ src/AC/Stack.h
@@ -1,7 +1,8 @@
 #include <CJ/Base.h>
 #include <CJ/Range.h>
 
-__attribute__((always_inline)) static inline CJRange ACStack_alloc(usize size)
-{
-    return CJRange_make((u8*)__builtin_alloca(size), size);
-}
+#define ACStack_alloc(size)                                     \
+    ({                                                          \
+        usize __size = (size);                                  \
+        CJRange_make((u8*)__builtin_alloca(__size), __size);    \
+    })
