92da2b07e58d14fba3408d848d7e3cf3e4d3815d
Junior Rantila <junior.rantila@gmail.com>
2023-06-22 00:14:51 +0200

    CJ: Add Arch

---
+++ src/CJ/Arch.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#if defined(_WIN32)
+#define __CJ_OS_windows 1
+#elif defined(__APPLE__)
+#define __CJ_OS_darwin 1
+#elif defined(__linux__)
+#define __CJ_OS_linux 1
+#else
+#error "Unknown OS"
+#endif
+#define OS(query) __CJ_OS_##query
+
+
+#if defined(__x86_64__)
+#define __CJ_ARCH_x86_64 1
+#else
+#error "Unknown cpu family"
+#endif
+#define ARCH(query) __CJ_ARCH_##query
